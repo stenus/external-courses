877f0eba8c09cf4ba8d6cb894cce85791e534106
Maxim Markin <markin3617@gmail.com>
2021-03-17 13:41:08 +0300

    Fix a magic number in task 13

--- src/ex4_js-objects-part2/task-13.js
+++ src/ex4_js-objects-part2/task-13.js
@@ -1,5 +1,7 @@
 function randomIntegerFrom0To100() {
-  return Math.floor(101 * Math.random());
+  const MIN = 0;
+  const MAX = 100;
+  return Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
 }
 
 module.exports = randomIntegerFrom0To100;
