485af7f36dd55e828e0e77ababe7379d3c4825a4
Maxim Markin <markin3617@gmail.com>
2021-03-23 10:02:40 +0300

    Allow the calculator to get Infinity in ex5_js-functions

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -3,30 +3,39 @@ const Calculator = (function () {
 
   return {
     add(value) {
-      if (Number.isFinite(value)) {
+      if (value !== undefined) {
         result += value;
       }
       return Calculator.add;
     },
 
     subtract(value) {
-      if (Number.isFinite(value)) {
+      if (value !== undefined) {
         result -= value;
       }
       return Calculator.subtract;
     },
 
     multiply(value) {
-      if (Number.isFinite(value)) {
+      if (value !== undefined) {
         result *= value;
       }
       return Calculator.multiply;
     },
 
     divide(value) {
-      if (Number.isFinite(value)) {
+      if (result === 0 && value === 0) {
+        throw new Error("Trying to divide 0 by 0");
+      }
+
+      if (result === Infinity && value === Infinity) {
+        throw new Error("Trying to divide Infinity by Infinity");
+      }
+
+      if (value !== undefined) {
         result /= value;
       }
+
       return Calculator.divide;
     },
 
